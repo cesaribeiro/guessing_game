b782bdd018b5a76f14d3123d10555bf3a245632c
Augusto Ribeiro <augcsr@live.com>
2020-05-30 15:33:13 -0300

    typing variables and formatting

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the name");
+    println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number: u32 = rand::thread_rng()
+        .gen_range(1, 101);
 
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
+        let mut guess: String = String::new();
 
         io::stdin()
             .read_line(&mut guess)
